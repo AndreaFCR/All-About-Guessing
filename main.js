e4c0704feb3f83267127c5100b4f9fa7d219fdc5
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-06-30 10:17:12 +0200

    add changes

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const button = document.querySelector(".js-button");
 const numberSelected = document.querySelector(".js-introNumber");
 const clueInput = document.querySelector(".js-clue");
 const randomNumber = getRandomNumber(100);
+console.log(randomNumber);
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
@@ -23,11 +24,25 @@ function handlerClick(ev) {
   numberValue();
   clueValue();
   console.log("esto funciona");
-//   if (randomNumber === getValue()) {
-//   }
-// }
-
-console.log(clueValue());
-console.log(getValue());
+  if (numberValue() >= 1 && numberValue() <= 100) {
+    console.log("esto funciona bien");
+    console.log(randomNumber);
+    console.log(numberValue());
+    console.log(clueValue());
+    if (numberValue() == randomNumber) {
+      console.log("has acertado");
+      console.log(clueValue());
+    } else if (numberValue() > randomNumber) {
+      console.log("demasiado alto");
+      console.log(clueValue());
+    } else if (numberValue() > randomNumber) {
+      console.log("ddemasiado bajo");
+      console.log(clueValue());
+    }
+  } else {
+    console.log("el número debe estar entre 1 y 100");
+    console.log(clueValue());
+  }
+}
 
 button.addEventListener("click", handlerClick);
