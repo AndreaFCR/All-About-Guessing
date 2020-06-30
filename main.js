3da49b23f230c5eaa112b14cc61053a35f8bec72
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-06-30 09:46:18 +0200

    add const and function listener

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 "use strict";
+const button = document.querySelector(".js-button");
+const numberSelected = document.querySelector(".js-introNumber");
+const clueInput = document.querySelector(".js-clue");
+const randomNumber = getRandomNumber(100);
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+function numberValue() {
+  const numberSelectedValue = numberSelected.value;
+  return numberSelectedValue;
+}
+
+function clueValue() {
+  const clueInputValue = clueInput.value;
+  return clueInputValue;
+}
+
+function handlerClick(ev) {
+  ev.preventDefault();
+  numberValue();
+  clueValue();
+  console.log("esto funciona");
+//   if (randomNumber === getValue()) {
+//   }
+// }
+
+console.log(clueValue());
+console.log(getValue());
+
+button.addEventListener("click", handlerClick);
