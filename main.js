60847ddb208c680729ba0d43aae540b31ffd7979
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-07-13 13:00:41 +0200

    add changes

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 "use strict";
 const button = document.querySelector(".js-button");
 const numberSelected = document.querySelector(".js-introNumber");
-const clueInput = document.querySelector(".js-clue");
+const clueText = document.querySelector(".js-clue");
+const counterText = document.querySelector(".js-counter");
 const randomNumber = getRandomNumber(100);
-console.log(randomNumber);
+let attempts = 0;
+
+console.log("Mi número aleatorio es:", randomNumber);
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
@@ -14,33 +17,29 @@ function numberValue() {
   return Number(numberSelectedValue);
 }
 
-function clueValue() {
-  const clueInputValue = clueInput.value;
-  return clueInputValue;
+function updateClue() {
+  if (numberValue() >= 1 && numberValue() <= 100) {
+    if (numberValue() === randomNumber) {
+      clueText.innerHTML = "Has ganado campeona!!";
+    } else if (numberValue() > randomNumber) {
+      clueText.innerHTML = "Pista: demasiado alto";
+    } else if (numberValue() < randomNumber) {
+      clueText.innerHTML = "Pista: demasiado bajo";
+    }
+  } else {
+    clueText.innerHTML = "Pista: El número debe estar entre 1 y 100";
+  }
+}
+function updateCounter() {
+  attempts += 1;
+  counterText.innerHTML = attempts;
 }
 
 function handlerClick(ev) {
   ev.preventDefault();
   numberValue();
-  clueValue();
-  console.log("esto funciona");
-  if (numberValue() >= 1 && numberValue() <= 100) {
-    console.log("se ha introducido un número del 1 al 100");
-    console.log(randomNumber); //lee el random number
-    console.log(numberValue()); //lee el valor introducido
-
-    if (numberValue() === randomNumber) {
-      console.log("has acertado");
-    } else if (numberValue() > randomNumber) {
-      console.log("demasiado alto");
-    } else if (numberValue() > randomNumber) {
-      console.log("demasiado bajo");
-    } else {
-      console.log("no está leyendo la equivalencia");
-    }
-  } else {
-    console.log("el número debe estar entre 1 y 100");
-  }
+  updateClue();
+  updateCounter();
 }
 
 button.addEventListener("click", handlerClick);
